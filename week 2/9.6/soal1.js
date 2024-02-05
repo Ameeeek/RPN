608c232470ce52d23621bd197b214c1bba01fcba
Ameeeek <ameeeekwork@gmail.com>
2024-02-05 22:25:21 +0800

    refactor

---
+++ week 2/9.6/soal1.js
@@ -0,0 +1,42 @@
+function changeMe(arr) {
+  // you can only write your code here!
+  if (arr.length == 0) return "";
+  let res = ''
+  for (let i = 0; i < arr.length; i++) {
+    res += `
+        ${i+1}. ${arr[i][0]} ${arr[i][1]}: 
+        firstName :${arr[i][0]},
+        lastName: ${arr[i][1]},
+        gender: ${arr[i][2]},
+        age: ${arr[i][3] == undefined ? "Invalid Birth Year" : 2023 - arr[i][3]}
+    `
+    
+    // res.push(person)
+  }
+  return res
+//   return res.map((a)=>{
+//     return `1. ${a.firstName} ${a.lastName} \n ${a}`
+//  })
+}
+
+// TEST CASES
+console.log(
+  changeMe([
+    ["Christ", "Evans", "Male", 1982],
+    ["Robert", "Downey", "Male"],
+  ])
+);
+// 1. Christ Evans:
+// Christ Evans:
+// { firstName: 'Christ',
+//   lastName: 'Evans',
+//   gender: 'Male',
+
+//   age: 41 } 2023 - 1982 = 41 kan yak wkwk
+// 2. Robert Downey:
+// { firstName: 'Robert',
+//   lastName: 'Downey',
+//   gender: 'Male',
+//   age: 'Invalid Birth Year' }
+
+changeMe([]); // ""
--- week 2/Logic Challenge lagi/soal3.js
+++ week 2/Logic Challenge lagi/soal3.js
@@ -1,41 +1,15 @@
 //TIPS: gunakan method toUpperCase() dan toLowerCase()
 function tukarBesarKecil(kalimat) {
-  return kalimat.includes("-")
-    ? kalimat
-        .split("-")
-        .map((a) => {
-          let res = "";
-          let split = a.split("");
-          for (let i = 0; i < split.length; i++) {
-            if (i == 0) {
-              res += split[i].toLowerCase();
-            } else {
-              res += split[i].toUpperCase();
-            }
-          }
-          return res;
-        })
-        .join("-")
-    : kalimat
-        .split(" ")
-        .map((a) => {
-          let res = "";
-          let split = a.split("");
-          for (let i = 0; i < split.length; i++) {
-            if (i == 0) {
-              res += split[i].toLowerCase();
-            } else {
-              res += split[i].toUpperCase();
-            }
-          }
-          return res;
-        })
-        .join(" ");
+  return kalimat
+    .split("")
+    .map((char) =>
+      char === "-" || char === " " ? char : char == char.toUpperCase() ? char.toLowerCase() : char.toUpperCase())
+    .join("");
 }
 
 // TEST CASES
-// console.log(tukarBesarKecil("Hello World")); // "hELLO wORLD"
-//   console.log(tukarBesarKecil('I aM aLAY')); // "i Am Alay"
-//   console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
-//   console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
+console.log(tukarBesarKecil("Hello World")); // "hELLO wORLD"
+console.log(tukarBesarKecil("I aM aLAY")); // "i Am Alay"
+console.log(tukarBesarKecil("My Name is Bond!!")); // "mY nAME IS bOND!!"
+console.log(tukarBesarKecil("IT sHOULD bE me")); // "it Should Be ME"
 console.log(tukarBesarKecil("001-A-3-5TrdYW")); // "001-a-3-5tRDyw"
