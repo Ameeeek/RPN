53f0b7c4303b17c96b7f5467450e36d6cb7ac952
Ameeeek <ameeeekwork@gmail.com>
2024-02-10 13:58:35 +0800

    fix soal 1 rekursif

--- week3/rekursif/soal1.js
+++ week3/rekursif/soal1.js
@@ -4,31 +4,12 @@ Implementasikan function sorting dan getTotal untuk mendapatkan angka yang palin
 Dengan HANYA mengubah code di dalam 2 function yang diberikan (sorting dan getTotal). Dilarang mengubah isi dalam function mostFrequentLargestNumbers!
 */
 function sorting(arrNumber) {
-  
-  if (arrNumber.length <= 1) { // ini base casenya, ngecek kalo panjang array itu kurang dari ato sama dengan satu ya artinya udah keurut ygy
-    return arrNumber;
-  }
-
-  // Pilih elemen tengah array sebagai pivot
-  let pivotIndex = Math.floor(arrNumber.length / 2);
-  let pivot = arrNumber[pivotIndex];
-  console.log(pivot)
-
-  // Bagi array menjadi dua bagian: yang lebih kecil dari pivot dan yang lebih besar dari pivot
-  const leftArray = [];
-  const rightArray = [];
-  for (let i = 0; i < arrNumber.length; i++) {
-    if (i !== pivotIndex) {
-      if (arrNumber[i] < pivot) {
-        leftArray.push(arrNumber[i]);
-      } else {
-        arrNumber.push(arrNumber[i]);
-      }
-    }
-  }
-
-  // Gabungkan hasil rekursif untuk kedua bagian array dengan pivot di tengahnya
-  return [...recursiveSort(leftArray), pivot, ...recursiveSort(rightArray)];
+  return arrNumber.sort((a,b)=> a - b)
+}
+function getTotal(arrNumber){
+  let max = Math.max(...arrNumber)
+  let mostOccur = arrNumber.filter((a)=> a== max).length
+  return `angka paling besar adalah ${max} dan muncul sebanyak ${mostOccur} kali`
 }
 
 function mostFrequentLargestNumbers(arrNumber) {
@@ -37,8 +18,8 @@ function mostFrequentLargestNumbers(arrNumber) {
   return countHighest;
 }
 
-// console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
-console.log(sorting([2, 8, 4, 6, 8, 5, 8, 4]));
+console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
+// console.log(sorting([2, 8, 4, 6, 8, 5, 8, 4]));
 //'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
 
 // console.log(mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150]));
